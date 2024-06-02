49d4e27793b30f4f559011fca262d688ce99558d
MamyNova69 <remicurie@gmail.com>
2024-06-02 16:48:44 +0200

    find_closest_seller

--- gasoline_prices.py
+++ gasoline_prices.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+from math import cos, asin, sqrt
+import json
 
 # note for english readers: pdv = point de vente (french for seller)
 
@@ -41,7 +43,7 @@ class gazoline_france:
         return response.text
 
     def prices(self, id_pdv): # retourn un dataframe panda
-        response = requests.get(f"{self.url}/map/recuperer_infos_pdv/"+id_pdv,
+        response = requests.get(f"{self.url}/map/recuperer_infos_pdv/"+str(id_pdv),
             cookies=self.cookies,
             headers=self.headers_to_get_datas,
         )
@@ -54,16 +56,41 @@ class gazoline_france:
         print(df)
         return df
     
-    def find_closest_seller(self, lat, lon):
+    def find_closest_seller(self, lat, lon): # return the id of the closest seller to the given point
         # en utilisant la formule de haversine
+        # https://fr.wikipedia.org/wiki/Formule_de_haversine
+        # a simple euclidean distance would be enough for small distances assuming earth is a plane, but for the sport and beauty we use haversine
+        # also someday we will run out of oil and we will need to know the distance between two points on the earth
+        
+        your_location = {'coordLatitude': lat, 'coordLongitude': lon}
 
-        pass
+        tempDataList = self.find_sellers()
+        dictionary = json.loads(tempDataList)
+
+        def distance(lat1, lon1, lat2, lon2):
+            p = 0.017453292519943295
+            haversin = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
+            return 12742 * asin(sqrt(haversin))
+        
+        def closest(data, v):
+            return min(data, key=lambda p: distance(float(v['coordLatitude']),float(v['coordLongitude']),float(p['coordLatitude']),float(p['coordLongitude'])))
+        
+        seller_info = closest(dictionary, your_location)
+
+        print(f"The closest seller to your point is : {seller_info}")
+        return seller_info['id']
+
 
 
 if __name__ == '__main__':
     gaz = gazoline_france()
-    print(gaz.find_sellers())
-    # html = gaz.prices('69007006')
+
+    id = gaz.find_closest_seller(45.740134, 4.838413)
+
+    prix = gaz.prices(id)
+
+    print(prix)
+
 
 
 
